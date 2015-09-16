10fd8cb36d7c49badb8d13b97a473ecab55590b2
ThanhDM <thanhdangminh36@gmail.com>
2015-09-16 17:10:20 +0700

    Add common files for getting rss

---
+++ FeedRSS/FetchArticleServices.m
@@ -0,0 +1,13 @@
+//
+//  FetchArticleServices.m
+//  FeedRSS
+//
+//  Created by ThanhDM on 9/16/15.
+//  Copyright (c) 2015 Training. All rights reserved.
+//
+
+#import "FetchArticleServices.h"
+
+@implementation FetchArticleServices
+
+@end
