80d1fd9745901cc74faf61393e640477dc9c9581
KinhNM1 <kinhnm1@iMac-wifi.local>
2015-09-18 14:29:41 +0700

    add for IT screen

---
+++ FeedRSS/ViewControllers/FRITViewController.m
@@ -0,0 +1,119 @@
+//
+//  FRHomeViewController.m
+//  FeedRSS
+//
+//  Created by KinhNM1 on 9/18/15.
+//  Copyright (c) 2015 Training. All rights reserved.
+//
+
+#import "FRITViewController.h"
+#import "FRDetailViewController.h"
+#import "HomeCell.h"
+
+@interface FRITViewController ()
+
+@end
+
+@implementation FRITViewController
+@synthesize table;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+     [self.table registerNib:[UINib nibWithNibName:NSStringFromClass([HomeCell class]) bundle:nil] forCellReuseIdentifier:@"HomeCell"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+/**LIST VIEW*/
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 100;
+    //return self.posts.count;
+    
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    
+    
+    {
+        HomeCell * cell = nil;
+        
+        static NSString *simpleTableIndentifier = @"HomeCell";
+        
+        
+        cell = (HomeCell *)[tableView dequeueReusableCellWithIdentifier:simpleTableIndentifier];
+        
+        if(cell == nil){
+            
+            NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"HomeCell" owner:self options:nil];
+            
+            cell = [nib objectAtIndex:0];
+            
+        }
+        //FRPost *post = [posts objectAtIndex:indexPath.row];
+        //cell.nameLabel.text = post.title;
+        cell.nameLabel.text = @" IT";
+        //cell.thumbnailImageView.contentMode = UIViewContentModeScaleToFill;
+        cell.thumbnailImageView.image = [UIImage imageNamed:@"husky.jpg"];
+
+        return cell;
+        
+    }
+    
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIAlertView *messageAlert = [[UIAlertView alloc]
+                                 initWithTitle:@"Row Selected" message:@"You've selected a row" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [self performSegueWithIdentifier:@"ViewITAction" sender:self];
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    
+    return 78;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    
+    NSLog(@"prepareForSegue");
+    
+    if ([segue.identifier isEqualToString:@"ViewITAction"]) {
+        
+        NSIndexPath *indexPath = [self.table indexPathForSelectedRow];
+        
+        FRDetailViewController *destViewController = segue.destinationViewController;
+        
+        destViewController.Url = @"http://www.dantri.com.vn";
+        
+    }
+    
+}
+
+@end
