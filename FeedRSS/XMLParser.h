890838e34315a28dba0d1ed420a8df0176d53679
HiepLT1 <hieplt1@HiepLT1.local>
2015-09-21 10:05:05 +0700

    no message

---
+++ FeedRSS/XMLParser.h
@@ -0,0 +1,13 @@
+//
+//  XMLParser.h
+//  FeedRSS
+//
+//  Created by HiepLT1 on 9/18/15.
+//  Copyright (c) 2015 Training. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLParser : NSObject
+
+@end
---
+++ FeedRSS/XMLParser.m
@@ -0,0 +1,13 @@
+//
+//  XMLParser.m
+//  FeedRSS
+//
+//  Created by HiepLT1 on 9/18/15.
+//  Copyright (c) 2015 Training. All rights reserved.
+//
+
+#import "XMLParser.h"
+
+@implementation XMLParser
+
+@end
